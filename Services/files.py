2f96f88ef72af286a818d379fe0a47d02714a93a
sgokul-cavintek <sgokul@cavintek.com>
2021-01-05 00:35:35 +0530

    code refactor

--- Services/files.py
+++ Services/files.py
@@ -6,6 +6,11 @@ IMAGES_UPLOAD_FOLDER = 'static/images/'
 API_URL = 'http://127.0.0.1:5000/'
 S3_PROP_IMAGES_URL = 'https://my-estate.s3-ap-southeast-1.amazonaws.com/images/properties/'
 S3_USER_IMAGES_URL = 'https://my-estate.s3-ap-southeast-1.amazonaws.com/images/users/'
+s3_client = boto3.client('s3',
+                         aws_access_key_id='AKIAI65DIV5RWERXPVLA',
+                         aws_secret_access_key='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',
+                         region_name='us-east-2'
+                         )
 
 
 def validateFile(fileName, extensions):
@@ -15,11 +20,6 @@ def validateFile(fileName, extensions):
 
 def insertUserImage(image, userId):
     if validateFile(image.filename, IMAGE_EXTENSIONS):
-        s3_client = boto3.client('s3',
-                                 aws_access_key_id='AKIAI65DIV5RWERXPVLA',
-                                 aws_secret_access_key='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',
-                                 region_name='us-east-2'
-                                 )
         filename = secure_filename(image.filename)
         s3_client.put_object(ACL='public-read', Body=image, Bucket='my-estate',
                              Key='images/users/' + str(userId) + '/' + filename)
@@ -29,23 +29,14 @@ def insertUserImage(image, userId):
 
 def insertPropertyImage(image, propertyId):
     if validateFile(image.filename, IMAGE_EXTENSIONS):
-        s3_client = boto3.client('s3',
-                                 aws_access_key_id='AKIAI65DIV5RWERXPVLA',
-                                 aws_secret_access_key='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',
-                                 region_name='us-east-2'
-                                 )
         filename = secure_filename(image.filename)
-        s3_client.put_object(ACL='public-read', Body=image, Bucket='my-estate', Key='images/properties/' + str(propertyId) + '/' + filename)
+        s3_client.put_object(ACL='public-read', Body=image, Bucket='my-estate',
+                             Key='images/properties/' + str(propertyId) + '/' + filename)
         return S3_PROP_IMAGES_URL + str(propertyId) + '/' + filename
     return False
 
 
 def removePropertyImage(imageName, propertyId):
-    s3_client = boto3.client('s3',
-                             aws_access_key_id='AKIAI65DIV5RWERXPVLA',
-                             aws_secret_access_key='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',
-                             region_name='us-east-2'
-                             )
     s3_client.delete_object(
         Bucket='my-estate',
         Key='images/properties/' + str(propertyId) + '/' + imageName,
